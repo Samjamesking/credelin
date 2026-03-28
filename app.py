253595b649d081cd8b282fd8f07392b6e5b0706c
Samjamesking <skgkargupta15@gmail.com>
2026-03-29 01:42:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -447,7 +447,7 @@ def main():
     if page == "Dashboard":
         dashboard(rf_model, encoders, config)
     elif page == "Single Prediction":
-        single_prediction(_rf_model, _encoders, config)
+        single_prediction(_rf_model, encoders, config)
     else:
         about()
 
